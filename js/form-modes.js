a512bf9db5648457d69edd98bb8dd20905bc50b4
dvovas <ddvovas@gmail.com>
2024-11-07 13:09:51 +0100

    Вносит изменения в код для включения и отключения формы

--- js/form-modes.js
+++ js/form-modes.js
@@ -1,33 +1,33 @@
-const form = document.querySelector('.ad-form');
-const formElements = form.querySelectorAll('.ad-form__element');
-const mapFilters = document.querySelectorAll('.map__filter');
+const adForm = document.querySelector('.ad-form');
+const mapForm = document.querySelector('.map__filters');
+const formElements = adForm.querySelectorAll('.ad-form__element');
+const mapFilters = mapForm.querySelectorAll('.map__filter');
 
 export const disableForm = () => {
-  form.classList.add('ad-form--disabled');
+  adForm.classList.add('ad-form--disabled');
 
-  form.querySelector('.ad-form-header__input').setAttribute('disabled', '');
+  adForm.querySelector('.ad-form-header__input').setAttribute('disabled', '');
   formElements.forEach((element) => {
     element.setAttribute('disabled', '');
   });
-
-  document.querySelector('.map__filters').classList.add('map__filters--disabled');
+  mapForm.classList.add('map__filters--disabled');
   mapFilters.forEach((filter) => {
     filter.setAttribute('disabled', '');
   });
-  document.querySelector('.map__features').setAttribute('disabled', '');
+  mapForm.querySelector('.map__features').setAttribute('disabled', '');
 };
 
 export const enableForm = () => {
-  form.classList.remove('ad-form--disabled');
-  form.querySelector('.ad-form-header__input').removeAttribute('disabled');
+  adForm.classList.remove('ad-form--disabled');
+  adForm.querySelector('.ad-form-header__input').removeAttribute('disabled');
 
   formElements.forEach((element) => {
     element.removeAttribute('disabled');
   });
 
-  document.querySelector('.map__filters').classList.remove('map__filters--disabled');
+  mapForm.classList.remove('map__filters--disabled');
   mapFilters.forEach((filter) => {
     filter.removeAttribute('disabled');
   });
-  document.querySelector('.map__features').removeAttribute('disabled', '');
+  mapForm.querySelector('.map__features').removeAttribute('disabled', '');
 };
